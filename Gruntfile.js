0db8cdf957ee276ddc935a4e68b86cd7fc6947de
pixeltight <jkerr013@gmail.com>
2016-10-28 08:14:58 -0400

    adding post css autoprefix to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,17 +17,28 @@ module.exports = function(grunt) {
 			}
 		},
 
+		autoprefixer: {
+			dist: {
+				files: {
+					'assets/css/scma_style.css': 'assets/css/cutestrap.css'
+				}
+			}
+		},
+
 		watch: {
             files: ['assets/scss/**/*.scss'],
             tasks: ['sass'],
+            files: ['assets/css/cutestrap.css'],
+            tasks: ['autoprefixer'],
             options: {
             	livereload: true
-            }
+            },
         }
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['sass', 'watch']);
 
